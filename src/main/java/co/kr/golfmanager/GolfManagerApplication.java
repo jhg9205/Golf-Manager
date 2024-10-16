321879e1de415660fc1c935f2e2d8a651b785cf8
jsh <jhg9205@nate.com>
2024-10-16 15:05:37 +0900

    2024-10-16 INIT

---
+++ src/main/java/co/kr/golfmanager/GolfManagerApplication.java
@@ -0,0 +1,13 @@
+package co.kr.golfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/golfmanager/GolfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.golfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
